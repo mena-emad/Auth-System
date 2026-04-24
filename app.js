15b7c9ef3daa7eef441a3ac3812681ccdeeb8ec5
Mena Emad <menaemad6543@gmail.com>
2026-04-24 18:54:26 +0300

    added cors settings

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ import gerror from "./utils/gerror.js";
 import router from "./modules/auth/auth.routes.js";
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true,
+  methods:["GET","POST","PUT","DELETE"],
+  allowedHeaders:["Content-Type","Authorization"]
+}));
 app.use(express.json());
 app.use(cookieParser());
 //routes
